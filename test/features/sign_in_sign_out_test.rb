77970facf87fde1432020385f1a0021a7bdb049d
Lee Yanco <lee.yanco@gmail.com>
2016-04-03 21:34:30 -0400

    Add SCSS boilerplate, break out devise views, and create basic header

--- test/features/sign_in_sign_out_test.rb
+++ test/features/sign_in_sign_out_test.rb
@@ -11,8 +11,11 @@ class SignInSignOutTest < Capybara::Rails::TestCase
   end
 
   test 'sign in and sign out' do
-    #layout should include name and email
-    visit new_user_registration_path
+    #Given it is my first time to the page
+    visit root_path
+    #When I sign up
+    click_link('Sign up')
+    #Then I should see the sign up form
     assert_content page, "Email"
     assert_content page, "Password"
     assert_content page, "Password confirmation"
@@ -20,17 +23,29 @@ class SignInSignOutTest < Capybara::Rails::TestCase
     fill_in('Email', with: 'test@example.com')
     fill_in('Password', with: 'ssssss')
     fill_in('Password confirmation', with: 'abcdefgh')
-    click_link_or_button('Sign up')
+    click_button('Sign up')
     #Then the signup should render again
     assert current_path, new_user_registration_path
     #When I enter good information
     fill_in('Email', with: 'test@example.com')
     fill_in('Password', with: 'abcdefgh')
     fill_in('Password confirmation', with: 'abcdefgh')
-    click_link_or_button('Sign up')
+    click_button('Sign up')
     #Then I should be signed up
     assert_content page, "Welcome! You have signed up successfully."
-
+    #When I click sign out
+    click_link('Sign out')
+    #Then I should be signed out
+    assert_content page, 'Signed out successfully.'
+    assert_no_content page, 'Sign out'
+    #When I click sign in
+    click_link('Sign in')
+    #And enter my credentials
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'abcdefgh')
+    click_button('Log in')
+    #Then I should be signed in
+    assert_content page, 'Signed in successfully.'
   end
 
 end
